6bac72dd67123cc246cef184157d03b229fead8b
mark_march <markofmarch@gmail.com>
2012-01-26 01:07:38 -0500

    new test cases for Account and SwordUtils

--- src/test/scala/org/ontrait/sword/SwordUtilSpecs.scala
+++ src/test/scala/org/ontrait/sword/SwordUtilSpecs.scala
@@ -1,24 +1,24 @@
 package org.ontrait.sword
 
+import scala.util.logging.ConsoleLogger
 import org.specs2.mutable._
 import java.io.File
 
-class SwordUtilsSpecs extends Specification {
-	"Sword Utils" should {
-		import SwordUtils._
+class SwordUtilsSpecs extends Specification{
+  "Sword Utils" should {
+    import SwordUtils._
 
-		"load property from property file" in {
-			val nonExistsFile = "nonExistsFile.properties"
-			val defaultFile = "default.properties"
-			// println(new File(defaultFile).getAbsolutePath)
+    "load property from property file" in {
+      val nonExistsFile = "nonExistsFile.properties"
+      val defaultFile = "default.properties"
 
-			loadProperty("wordnik.api.key", "") must not be equalTo("")
+      loadProperty("wordnik.api.key", "") must not be equalTo("")
 
-			loadProperty("wordnik.api.key", "")(defaultFile) must not be equalTo("")
+      loadProperty("wordnik.api.key", "")(defaultFile) must not be equalTo("")
 
-			loadProperty("wordnik.api.key", "")(nonExistsFile) must be equalTo("")
+      loadProperty("wordnik.api.key", "")(nonExistsFile) must be equalTo("")
 
-			loadProperty("strange.key", "") must be equalTo("")
-		}
-	}
+      loadProperty("strange.key", "") must be equalTo("")
+    }
+  }
 }
