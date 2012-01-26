b026287b19e14aa8e50d5cd2a6c6a87abd6c3068
mark_march <markofmarch@gmail.com>
2012-01-25 20:08:44 -0500

    deleted test files

--- src/main/scala/HelloWorld.scala
+++
@@ -1,5 +0,0 @@
-object HelloWorld extends App {
-  println("Hello")
-
-  def sayHello(name: String): String = "Hello " + name + "!"
-}
--- src/test/scala/HelloWordSpec.scala
+++
@@ -1,9 +0,0 @@
-import org.specs2.mutable._
-
-object HelloWordSpec extends Specification {
-  "Hello World" should {
-    "say hello to anybody" in {
-      HelloWorld.sayHello("World") must beEqualTo("Hello World!")
-    }
-  }
-}
