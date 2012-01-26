327446f374e0416ffa07d20e9a27a451b536e607
mark_march <markofmarch@gmail.com>
2012-01-26 01:11:20 -0500

    converted tabs to spaces

--- src/main/scala/org/ontrait/sword/Account.scala
+++ src/main/scala/org/ontrait/sword/Account.scala
@@ -7,28 +7,28 @@ import dispatch.liftjson.Js._
 import net.liftweb.json._
 
 object Authentication {
-	def apply(username: String, password: String) = new AuthenticationBuilder(username, password)
+  def apply(username: String, password: String) = new AuthenticationBuilder(username, password)
 
-	private[sword] class AuthenticationBuilder(username: String, password: String) extends ObjectQueryMethod {
-		def complete = _ / "account.json" / "authenticate"/ username <<? Map("password" -> password)
-	}
+  private[sword] class AuthenticationBuilder(username: String, password: String) extends ObjectQueryMethod {
+    def complete = _ / "account.json" / "authenticate"/ username <<? Map("password" -> password)
+  }
 
-	val message = 'message ? str
-	val token = 'token ? str
-	val userId = 'userId ? int
-	val userSignature = 'userSignature ? str  
+  val message = 'message ? str
+  val token = 'token ? str
+  val userId = 'userId ? int
+  val userSignature = 'userSignature ? str  
 }
 
 object ApiTokenStatus {
-	def apply(apiKey: String) = new ObjectQueryMethod {
-		def complete = _ / "account.json" / "apiTokenStatus" <<? Map("api_key" -> apiKey)
-	}
+  def apply(apiKey: String) = new ObjectQueryMethod {
+    def complete = _ / "account.json" / "apiTokenStatus" <<? Map("api_key" -> apiKey)
+  }
 
-	val valid = 'valid ? bool
-	val token = 'token ? str
-	val expireDate = 'expiresInMillis ? date
-	val remainingCalls = 'remainingCalls ? int
-	val totalRequests = 'totalRequests ? int
-	val resetsInMillis = 'resetsInMillis ? int
+  val valid = 'valid ? bool
+  val token = 'token ? str
+  val expireDate = 'expiresInMillis ? date
+  val remainingCalls = 'remainingCalls ? int
+  val totalRequests = 'totalRequests ? int
+  val resetsInMillis = 'resetsInMillis ? int
 }
 
--- src/test/scala/org/ontrait/sword/AccountSpec.scala
+++ src/test/scala/org/ontrait/sword/AccountSpec.scala
@@ -5,43 +5,43 @@ import dispatch._
 import liftjson.Js._
 
 class AccountSpec extends Specification {
-	import SwordUtils._
-	val apiClient = ApiClient(loadProperty("wordnik.api.key", ""))
+  import SwordUtils._
+  val apiClient = ApiClient(loadProperty("wordnik.api.key", ""))
 
-	"Authentication" should {
-		"verify user crendentials" in {
-			val http = new Http
-			val token = http(apiClient(Authentication("sword", "erhuaming")) ># (Authentication.token)).headOption
-			token.get.length must be > (0)
+  "Authentication" should {
+    "verify user crendentials" in {
+      val http = new Http
+      val token = http(apiClient(Authentication("sword", "erhuaming")) ># (Authentication.token)).headOption
+      token.get.length must be > (0)
 
-			{ 
-				http(apiClient(Authentication("sword", "wrongpassword")) ># (Authentication.message))
-			} must throwA[Exception].like {
-				case e => e.getMessage must contain ("Invalid password")
-			}
+      { 
+        http(apiClient(Authentication("sword", "wrongpassword")) ># (Authentication.message))
+      } must throwA[Exception].like {
+        case e => e.getMessage must contain ("Invalid password")
+      }
 
-			{
-				http(apiClient(Authentication("swordabc", "wrongpassword")) ># (Authentication.message))
-			} must throwA[Exception].like {
-				case e => e.getMessage must contain ("user not found")
-			}
-		}
-	}
+      {
+        http(apiClient(Authentication("swordabc", "wrongpassword")) ># (Authentication.message))
+      } must throwA[Exception].like {
+        case e => e.getMessage must contain ("user not found")
+      }
+    }
+  }
 
-	"ApiTokenStatus" should {
-		"check api token status" in {
-			val http = new Http
-			val client = new WordnikClient{
-				def apply(block: Request => Request): Request = block(host)
-			}
-			val valid = http(client(ApiTokenStatus(loadProperty("wordnik.api.key", "")))># (ApiTokenStatus.valid)).headOption
-			valid.get must beTrue
+  "ApiTokenStatus" should {
+    "check api token status" in {
+      val http = new Http
+      val client = new WordnikClient{
+        def apply(block: Request => Request): Request = block(host)
+      }
+      val valid = http(client(ApiTokenStatus(loadProperty("wordnik.api.key", "")))># (ApiTokenStatus.valid)).headOption
+      valid.get must beTrue
 
-			{
-				http(client(ApiTokenStatus("somerandomtext")) ># (ErrorResponse.message))
-			} must throwA[Exception].like {
-				case e => e.getMessage must contain ("unauthorized")
-			}
-		}
-	}
+      {
+        http(client(ApiTokenStatus("somerandomtext")) ># (ErrorResponse.message))
+      } must throwA[Exception].like {
+        case e => e.getMessage must contain ("unauthorized")
+      }
+    }
+  }
 }
