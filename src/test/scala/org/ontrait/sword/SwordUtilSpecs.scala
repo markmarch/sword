dce766d18af983f74873611bc0241f74bab39b75
mark_march <markofmarch@gmail.com>
2012-01-25 20:10:32 -0500

    test for SwordUtils

---
+++ src/test/scala/org/ontrait/sword/SwordUtilSpecs.scala
@@ -0,0 +1,24 @@
+package org.ontrait.sword
+
+import org.specs2.mutable._
+import java.io.File
+
+class SwordUtilsSpecs extends Specification {
+	"Sword Utils" should {
+		import SwordUtils._
+
+		"load property from property file" in {
+			val nonExistsFile = "nonExistsFile.properties"
+			val defaultFile = "default.properties"
+			// println(new File(defaultFile).getAbsolutePath)
+
+			loadProperty("wordnik.api.key", "") must not be equalTo("")
+
+			loadProperty("wordnik.api.key", "")(defaultFile) must not be equalTo("")
+
+			loadProperty("wordnik.api.key", "")(nonExistsFile) must be equalTo("")
+
+			loadProperty("strange.key", "") must be equalTo("")
+		}
+	}
+}
