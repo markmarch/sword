e79b83d4b880b851f63eb28f2ab44e20d73430bb
mark_march <markofmarch@gmail.com>
2012-01-27 16:41:47 -0500

    Added wordnikDateFormat and ThreadLocal class

--- src/main/scala/org/ontrait/sword/package.scala
+++ src/main/scala/org/ontrait/sword/package.scala
@@ -3,5 +3,13 @@ package org.ontrait
 import dispatch.liftjson.Js._
 
 package object sword {
-  val wdate = datestr("yyyy-MM-DD'T'HH:mm:ss.SSSZZZZ")
+  // date format for wordnik api
+  val wordnikDateFormat = "yyyy-MM-DD'T'HH:mm:ss.SSSZZZZ"
+  val wdate = datestr(wordnikDateFormat)
+
+  // taken form lift json library
+  class ThreadLocal[A](init: => A) extends java.lang.ThreadLocal[A] with (() => A) {
+    override def initialValue = init
+    def apply = get
+  }
 }
