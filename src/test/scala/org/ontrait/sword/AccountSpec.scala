41ff1850390cfc4c9ffa067f87f1786fb3700bd2
mark_march <markofmarch@gmail.com>
2012-01-27 16:43:18 -0500

    Added wordLists specs

--- src/test/scala/org/ontrait/sword/AccountSpec.scala
+++ src/test/scala/org/ontrait/sword/AccountSpec.scala
@@ -61,4 +61,40 @@ class AccountSpec extends Specification {
       }
     }
   }
+  
+  "WordLists" should { 
+    "extract WordList object from json" in {
+      val json = parse("""
+        {
+          "type":"PRIVATE",
+          "userId":1082314,
+          "username":"sword",
+          "permalink":"test--44",
+          "numberWordsInList":1,
+          "lastActivityAt":"2012-01-27T20:21:20.138+0000",
+          "updatedAt":"2012-01-27T20:21:20.166+0000",
+          "createdAt":"2012-01-27T20:21:20.138+0000",
+          "description":"testing",
+          "name":"test",
+          "id":71020
+        }""")
+      WordList.get(json) match {
+        case Right(wordList) => wordList.username aka "username of the wordlist" must be equalTo "sword"
+        case Left(e) => 1 aka e.getMessage must be > (1)
+      }
+    }
+
+    "fetch user's word lists" in {
+      val res = http(apiClient.handle(WordLists(loadProperty("wordnik.auth.token"))))
+      res.size aka "nuber of lists" must be > (1)
+      res map { WordList.get } forall { _.isRight } must beTrue
+      res map { WordList.get } forall { _.right.get.username == "sword"} must beTrue
+    }
+
+    "limit fetched user's word lists to be less or equal to 1" in {
+      val res = http(apiClient.handle(WordLists(loadProperty("wordnik.auth.token")).limit(1)))
+      res.size aka "number of lists" must be > (0)
+      res.size aka "number of lists" must be < (2)
+    }
+  }
 }
