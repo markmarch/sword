0cace5c689cefaf1438c296a9437eff5eafc6e7e
mark_march <markofmarch@gmail.com>
2012-01-28 22:36:55 -0500

    Added word lists api and spec

---
+++ src/main/scala/org/ontrait/sword/wordlist.scala
@@ -0,0 +1,112 @@
+package org.ontrait.sword
+
+import dispatch._
+import dispatch.Request._
+import dispatch.liftjson._
+import dispatch.liftjson.Js._
+
+import net.liftweb.json._
+import java.util.Date
+
+// wordLists
+case class WordLists(authToken: String) {
+  val contentType = "application/json"
+
+  // get word list by user auth token
+  object ByUser {
+    def apply() = new ByUserBuilder(Map()).authToken(authToken)
+    
+    private[sword] class ByUserBuilder(params: Map[String, String]) extends ListQueryMethod with AuthToken[ByUserBuilder]{
+      protected def param[T](key: String)(value: T) = new ByUserBuilder(params + (key -> value.toString))
+      val skip = param[Int]("skip") _
+      val limit = param[Int]("limit") _
+
+      def complete = _ / "account.json" / "wordLists" <<? params
+    }
+  }  
+
+  object ById {
+    def apply(listId: String) = new ObjectQueryMethod {
+      def complete = _ / "wordList.json" / listId <<? Map("auth_token" -> authToken)
+    }
+  }
+
+  object Words {
+    def apply(listId: String) = new WordsBuilder(listId, Map()).authToken(authToken)
+
+    private[sword] class WordsBuilder(listId: String, params: Map[String, String]) extends ListQueryMethod with AuthToken[WordsBuilder] with HasOrder[WordsBuilder] {
+      protected def param[T](key: String)(value: T) = new WordsBuilder(listId, params + (key -> value.toString))
+
+      val skip = param[Int]("skip") _
+      val limit = param[Int]("limit") _
+
+      def complete = _ / "wordList.json" / listId / "words" <<? params
+    }
+  }
+
+  object Add {
+    def apply(listId: String, words: Iterable[String]) = new ListQueryMethod {
+      def complete = _ / "wordList.json" / listId / "words" << (toJsonStr(words), contentType) <<? Map("auth_token" -> authToken)
+    }
+  }
+
+  object DeleteWords {
+    def apply(listId: String, words: Iterable[String]) = new ListQueryMethod {
+      def complete = _ / "wordList.json" / listId / "deleteWords" << (toJsonStr(words), contentType) <<? Map("auth_token" -> authToken)
+    }
+  }
+
+  object Delete {
+    def apply(listId: String) = new ObjectQueryMethod {
+      def complete = _.DELETE / "wordList.json" / listId <<? Map("auth_token" -> authToken) 
+    }
+  }
+
+  object Update {
+    def apply(listId: String, list: WordList) = new ObjectQueryMethod {
+      def complete = new Function1[Request, Request] {
+        def apply(r: Request) = {
+          r.PUT.copy(body = Some(new RefStringEntity(list.toJsonStr, contentType, r.defaultCharset))) / "wordList.json" / listId <<? Map("auth_token" -> authToken) 
+        }
+      }
+    }
+  }
+}
+
+// word list words item
+object WordListItem extends Extractor[WordListItem] {
+  val userId = 'userId ? int
+  val username = 'username ? str
+  val numberCommentsOnWord = 'numberCommentsOnWord ? int
+  val numberLists = 'numberLists ? int
+  val createdAt = 'createdAt ? wdate
+  val id = 'id ? int
+  val word = 'word ? str
+}
+
+case class WordListItem(userId: Long, username: String, numberCommentsOnWord: Int, numberLists: Int, createdAt: Date, id: Long, word: String)
+
+sealed abstract trait Type extends JString
+object Public extends JString("PUBLIC") with Type
+object Private extends JString("PRIVATE") with Type
+
+object WordList extends Extractor[WordList] {
+  val `type` = 'type ? in(Public, Private)
+  val userId = 'userId ? int
+  val username= 'username ? str
+  val permalink = 'permalink ? str
+  val numberWordsInList = 'numberWordsInList ? int
+  val lastActivityAt = 'lastActivityAt ? wdate
+  val updatedAt = 'updatedAt ? wdate
+  val createdAt = 'createdAt ? wdate
+  val description = 'description ? str
+  val name = 'name ? str
+  val id = 'id ? int
+}
+
+case class WordList(`type`: String, userId: Long, username: String, permalink: String, numberWordsInList: Int, lastActivityAt: Date, updatedAt: Date, createdAt: Date, description: String, name: String, id: Long) {
+  import net.liftweb.json._
+  import Serialization._
+  implicit val formats = Serialization.formats(ShortTypeHints(List(classOf[WordList])))
+  def toJsonStr = write(this)
+}
---
+++ src/test/scala/org/ontrait/sword/WordListSpec.scala
@@ -0,0 +1,93 @@
+package org.ontrait.sword
+
+import org.specs2.mutable._
+import dispatch._
+import dispatch.Request._
+import dispatch.liftjson._
+import dispatch.liftjson.Js._
+import net.liftweb.json._
+import java.util.Date
+
+class WordListSpec extends Specification with TestClient {
+  val permalink = "third--1"
+
+  "WordLists" should { 
+    "extract WordList object from json" in {
+      val json = parse("""
+        { 
+          "type":"PRIVATE",
+          "userId":1082314,
+          "username":"sword",
+          "permalink":"test--44",
+          "numberWordsInList":1,
+          "lastActivityAt":"2012-01-27T20:21:20.138+0000",
+          "updatedAt":"2012-01-27T20:21:20.166+0000",
+          "createdAt":"2012-01-27T20:21:20.138+0000",
+          "description":"testing",
+          "name":"test",
+          "id":71020
+        }""")
+      WordList.get(json) match {
+        case Right(wordList) => wordList.username aka "username of the wordlist" must be equalTo "sword"
+        case Left(e) => 1 aka e.getMessage must be > (1)
+      }
+    }
+
+    "fetch user's word lists" in {
+      val res = fetch(WordLists(authToken).ByUser())
+      res.size aka "nuber of lists" must be > (0)
+      res map { WordList.get } forall { _.isRight } must beTrue
+      res map { WordList.get(_).right.get } forall { _.username == "sword"} must beTrue
+    }
+
+    "limit fetched user's word lists to be less or equal to 1" in {
+      val res = fetch(WordLists(authToken).ByUser().limit(1))
+      res.size aka "number of lists" must be > (0)
+      res.size aka "number of lists" must be < (2)
+    }
+
+    "fetches words in a WordList and sort by alpha" in {
+      val res = fetch(WordLists(authToken).Words(permalink).sortBy(Alpha))
+      res.size must be > (0)
+      res map { WordListItem.get(_).right.get } forall {_.username == "sword"}
+    }
+
+    "add words to a word list" in {
+      fetch(WordLists(authToken).Add(permalink, List("random")))
+      success
+    }
+
+    "fetchs a word list by id" in {
+      val res = fetch(WordLists(authToken).ById(permalink))
+      WordList.get(res) must beRight.like {
+        case l: WordList => l.permalink must be equalTo (permalink)
+      }
+    }
+
+    "delete words from a word list" in {
+      fetch(WordLists(authToken).DeleteWords(permalink, List("random")))
+      success
+    }
+
+    "update a word list" in {
+      val name = util.Random.nextInt(10) + "name"
+      val old = fetch(WordLists(authToken).ById(permalink))
+      val list = WordList.get(old).right.get
+      fetch(WordLists(authToken).Update(permalink, list.copy(name=name)))
+
+      val res = fetch(WordLists(authToken).ById(permalink))
+      WordList.get(res) must beRight.like {
+        case wl: WordList => wl.name must be equalTo (name)
+      }
+    }
+  }
+
+  "WordListItem" should {
+    "extract WordListItem object from json" in {
+      val json = parse("""{"userId":1082314,"username":"sword","numberCommentsOnWord":2,"numberLists":32,"createdAt":"2012-01-28T23:24:38.782+0000","id":1120557,"word":"sword"}""")
+      WordListItem.get(json) must beRight.like {
+        case i: WordListItem => i.id must be_== (1120557L)
+      }
+    }
+  }
+}
