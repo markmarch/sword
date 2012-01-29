15abce184d70553b8a930d6c833e8cd3549bc773
mark_march <markofmarch@gmail.com>
2012-01-28 22:40:07 -0500

    Added method to store properties

--- src/main/scala/org/ontrait/sword/utils.scala
+++ src/main/scala/org/ontrait/sword/utils.scala
@@ -2,7 +2,7 @@ package org.ontrait.sword
 
 import scala.util.logging.ConsoleLogger 
 import java.util.Properties
-import java.io.{FileInputStream, File}
+import java.io.{FileInputStream, FileWriter, File}
 
 object SwordUtils extends ConsoleLogger{
   implicit val defaultPropsFile = "default.properties"
@@ -25,5 +25,15 @@ object SwordUtils extends ConsoleLogger{
       case e: Exception => log(e.getMessage)
     }
     props
+  }
+
+  def storeProps(props: Properties)(implicit propsFile: String) = {
+    val fs = new FileWriter(propsFile)
+    try {
+      props.store(fs, "")
+      fs.close
+    } catch {
+      case e => log(e.getMessage)
+    }
   }
 }
