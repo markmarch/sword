5cc2ef288477f990a978fb1f2dbbc8ff6c773fb9
mark_march <markofmarch@gmail.com>
2012-01-28 22:40:25 -0500

    Updated Account spec

--- src/test/scala/org/ontrait/sword/AccountSpec.scala
+++ src/test/scala/org/ontrait/sword/AccountSpec.scala
@@ -6,44 +6,56 @@ import liftjson.Js._
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
 
-class AccountSpec extends Specification {
+class AccountSpec extends Specification  with TestClient {
   import SwordUtils._
-  val apiClient = ApiClient(loadProperty("wordnik.api.key", ""))
-  val http = new Http
-  val props = getProps
 
   // Authentication test would invlaid the auth token stroed for other test
-  // "Authentication" should {
-  //   "verify user crendentials" in {
-  //     val http = new Http
-  //     val token = http(apiClient(Authentication("sword", "erhuaming")) ># (Authentication.token)).head
-  //     token.length must be > (0)
-  //     props.setProperty("wordnik.auth.token", token)
+  "Authenticate" should {
+    "verify user crendentials(GET)" in {
+      val res = fetch(Authenticate(username,  password))
+      Authenticate.get(res) must beRight.like {
+        case auth: Authenticate => 
+          setAuthToken(auth.token)
+          storeProps(props)
+          auth.token.length must be > (0)
+      }
 
-  //     { 
-  //       http(apiClient(Authentication("sword", "wrongpassword")) ># (Authentication.message))
-  //     } must throwA[Exception].like {
-  //       case e => e.getMessage must contain ("Invalid password")
-  //     }
+      { 
+        fetch(Authenticate("sword", "wrongpassword"))
+      } must throwA[Exception].like {
+        case e => e.getMessage must contain ("Invalid password")
+      }
 
-  //     {
-  //       http(apiClient(Authentication("swordabc", "wrongpassword")) ># (Authentication.message))
-  //     } must throwA[Exception].like {
-  //       case e => e.getMessage must contain ("user not found")
-  //     }
-  //   }
-  // }
+      {
+        fetch(Authenticate("_random_no_&abc", "admin"))
+      } must throwA[Exception].like {
+        case e => e.getMessage must contain ("user not found")
+      }
+    }
+
+    "verify user crendentials(POST)" in {
+      val res = fetch(Authenticate(username, password).POST)
+      Authenticate.get(res) must beRight.like {
+        case auth: Authenticate => 
+          setAuthToken(auth.token)
+          storeProps(props)
+          auth.token.length must be > (0)
+      }
+    }
+  }
 
   "ApiTokenStatus" should {
     "check api token status" in {
       val client = new WordnikClient{
         def apply(block: Request => Request): Request = block(host)
       }
-      val valid = http(client(ApiTokenStatus(loadProperty("wordnik.api.key", "")))># (ApiTokenStatus.valid)).headOption
-      valid.get must beTrue
+      val res = http(client.handle(ApiTokenStatus(apiKey)))
+      ApiTokenStatus.get(res) must beRight.like {
+        case s: ApiTokenStatus => s.valid must beTrue
+      }
 
       {
-        http(client(ApiTokenStatus("somerandomtext")) ># (ErrorResponse.message))
+        http(client.handle(ApiTokenStatus("somerandomtext")))
       } must throwA[Exception].like {
         case e => e.getMessage must contain ("unauthorized")
       }
@@ -52,50 +64,16 @@ class AccountSpec extends Specification {
 
   "User" should {
     "fetch user info" in {
-      val username = http(apiClient(User(loadProperty("wordnik.auth.token", ""))) ># ( User.username )).headOption
-      username.get must be equalTo ("sword")
+      val res = fetch(User(authToken))
+      User.get(res) must beRight.like {
+        case u: User => u.username must be equalTo ("sword")
+      }
 
       {
-        http(apiClient(User("wrongtoken")) ># (ErrorResponse.message)) 
+        fetch(User("wrong_token"))
       } must throwA[Exception].like {
         case e => e.getMessage must contain ("User not logged in")
       }
-    }
-  }
-
-  "WordLists" should { 
-    "extract WordList object from json" in {
-      val json = parse("""
-        {
-          "type":"PRIVATE",
-          "userId":1082314,
-          "username":"sword",
-          "permalink":"test--44",
-          "numberWordsInList":1,
-          "lastActivityAt":"2012-01-27T20:21:20.138+0000",
-          "updatedAt":"2012-01-27T20:21:20.166+0000",
-          "createdAt":"2012-01-27T20:21:20.138+0000",
-          "description":"testing",
-          "name":"test",
-          "id":71020
-        }""")
-      WordList.get(json) match {
-        case Right(wordList) => wordList.username aka "username of the wordlist" must be equalTo "sword"
-        case Left(e) => 1 aka e.getMessage must be > (1)
-      }
-    }
-
-    "fetch user's word lists" in {
-      val res = http(apiClient.handle(WordLists(loadProperty("wordnik.auth.token"))))
-      res.size aka "nuber of lists" must be > (1)
-      res map { WordList.get } forall { _.isRight } must beTrue
-      res map { WordList.get } forall { _.right.get.username == "sword"} must beTrue
-    }
-
-    "limit fetched user's word lists to be less or equal to 1" in {
-      val res = http(apiClient.handle(WordLists(loadProperty("wordnik.auth.token")).limit(1)))
-      res.size aka "number of lists" must be > (0)
-      res.size aka "number of lists" must be < (2)
     }
   }
 }
