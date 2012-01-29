77c97f09ca227d86a4df27c7de1bf70534de4149
mark_march <markofmarch@gmail.com>
2012-01-28 22:40:46 -0500

    minor changes

--- src/test/scala/org/ontrait/sword/TestClient.scala
+++ src/test/scala/org/ontrait/sword/TestClient.scala
@@ -5,8 +5,19 @@ import dispatch.Http
 trait TestClient {
   import SwordUtils._
 
-  val apiClient = ApiClient(loadProperty("wordnik.api.key", ""))
+  // props
+  val props = getProps
+  def username = props.getProperty("wordnik.username", "")
+  def password = props.getProperty("wordnik.password", "")
+  def authToken = props.getProperty("wordnik.auth.token", "")
+  def apiKey = props.getProperty("wordnik.api.key", "")
+
+  def setAuthToken(token: String) = props.setProperty("wordnik.auth.token", token)
+
+  val apiClient = ApiClient(apiKey)
   val http = new Http
 
   def fetch[T](method: Method[T]) = http(apiClient.handle(method))
+
+  def post[T](method: Method[T]) = http(apiClient(method) >|)
 }
